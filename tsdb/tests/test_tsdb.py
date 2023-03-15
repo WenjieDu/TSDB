f26e6e1c3fc988dee30e7c9684d4fd81f2ee5e47
Wenjie Du <wenjay.du@gmail.com>
2023-03-15 16:22:29 +0800

    fix: adjust the order of the test cases;

--- tsdb/tests/test_tsdb.py
+++ tsdb/tests/test_tsdb.py
@@ -19,21 +19,21 @@ DATASETS_TO_TEST = [
 
 
 class TestTSDB(unittest.TestCase):
-    def test_available_datasets(self):
+    def test_0_available_datasets(self):
         available_datasets = tsdb.list_available_datasets()
         assert len(available_datasets) > 0
 
-    def test_dataset_loading(self):
-        for d_ in DATASETS_TO_TEST:
-            data = tsdb.load_dataset(d_)
-            assert isinstance(data, dict)
-
-    def test_downloading_only(self):
+    def test_1_downloading_only(self):
         tsdb.download_and_extract('UCR_UEA_Wine', './save_it_here')
         file_list = os.listdir()
         assert len(file_list) > 0
 
-    def test_dataset_purging(self):
+    def test_2_dataset_loading(self):
+        for d_ in DATASETS_TO_TEST:
+            data = tsdb.load_dataset(d_)
+            assert isinstance(data, dict)
+
+    def test_3_dataset_purging(self):
         cached_datasets = tsdb.list_cached_data()
         assert isinstance(cached_datasets, list)
         tsdb.delete_cached_data('physionet_2012')  # delete single
